48dd1999e4202ddf7d86daa3db0b461934de31c3
Ricardo <ricardo.lima.messias@gmail.com>
2025-07-03 21:10:34 -0300

    Update Principal.java

--- src/principal/Principal.java
+++ src/principal/Principal.java
@@ -29,63 +29,66 @@ public class Principal {
 
             opcao = teclado.nextInt();
 
-    if (opcao==1){
+            if (opcao==1){
 //                dolar para real
-        System.out.println("Quantos DOLARES sera convertido para REAL?");
-        Conversor conversor = new Conversor();
-        double valor = teclado.nextDouble();
-        conversor.setValorParaConverter(valor);
-        conversor.dolarParaReal();
+                System.out.println("Quantos DOLARES sera convertido para REAL?");
+                Conversor conversor = new Conversor();
+                double valor = teclado.nextDouble();
+                conversor.setValorParaConverter(valor);
+                conversor.valorConvertido("USD","BRL");
 
-    } else if (opcao==2) {
+
+
+            } else if (opcao==2) {
 //                real para dolar
-        System.out.println("Quantos REAIS sera convertido para DOLAR?");
-        Conversor conversor = new Conversor();
-        double valor = teclado.nextDouble();
-        conversor.setValorParaConverter(valor);
-        conversor.realParaDolar();
+                System.out.println("Quantos REAIS sera convertido para DOLAR?");
+                Conversor conversor = new Conversor();
+                double valor = teclado.nextDouble();
+                conversor.setValorParaConverter(valor);
+                conversor.valorConvertido("BRL","USD");
 
 
-    } else if (opcao==3) {
+            } else if (opcao==3) {
 //                dolar para euro
-        System.out.println("Quantos DOLARES sera convertido para EURO?");
-        Conversor conversor = new Conversor();
-        double valor = teclado.nextDouble();
-        conversor.setValorParaConverter(valor);
-        conversor.dolarParaEuro();
+                System.out.println("Quantos DOLARES sera convertido para EURO?");
+                Conversor conversor = new Conversor();
+                double valor = teclado.nextDouble();
+                conversor.setValorParaConverter(valor);
+                conversor.valorConvertido("USD","EUR");
 
 
-    } else if (opcao==4) {
+            } else if (opcao==4) {
 //                euro para dolar
-        System.out.println("Quantos EUROS sera convertido para DOLAR?");
-        Conversor conversor = new Conversor();
-        double valor = teclado.nextDouble();
-        conversor.setValorParaConverter(valor);
-        conversor.euroParaDolar();
+                System.out.println("Quantos EUROS sera convertido para DOLAR?");
+                Conversor conversor = new Conversor();
+                double valor = teclado.nextDouble();
+                conversor.setValorParaConverter(valor);
+                conversor.valorConvertido("EUR","USD");
 
-    } else if (opcao==5) {
+
+            } else if (opcao==5) {
 //                dolar para peso
-        System.out.println("Quantos DOLARES sera convertido para PESO?");
-        Conversor conversor = new Conversor();
-        double valor = teclado.nextDouble();
-        conversor.setValorParaConverter(valor);
-        conversor.dolarParaPeso();
+                System.out.println("Quantos DOLARES sera convertido para PESO?");
+                Conversor conversor = new Conversor();
+                double valor = teclado.nextDouble();
+                conversor.setValorParaConverter(valor);
+                conversor.valorConvertido("USD","ARS");
 
 
-    } else if (opcao==6) {
+            } else if (opcao==6) {
 //                peso para dolar
-        System.out.println("Quantos PESOS sera convertido para DOLAR?");
-        Conversor conversor = new Conversor();
-        double valor = teclado.nextDouble();
-        conversor.setValorParaConverter(valor);
-        conversor.pesoParaDolar();
+                System.out.println("Quantos PESOS sera convertido para DOLAR?");
+                Conversor conversor = new Conversor();
+                double valor = teclado.nextDouble();
+                conversor.setValorParaConverter(valor);
+                conversor.valorConvertido("ARS","USD");
 
 
-    } else if (opcao < 0 || opcao > 6) {
+            } else if (opcao < 0 || opcao > 6) {
 
-        System.out.println("Numero invalido, digite uma opção valida.");
+                System.out.println("Numero invalido, digite uma opção valida.");
 
-    }
+            }
 
 
 
